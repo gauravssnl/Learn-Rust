d0db74a5095fe8b923b13b67dc01287a5f5a965d
Gaurav <gaurav.ssnl@gmail.com>
2020-06-06 12:12:34 +0530

    Added example to drop a value early by using std::mem::drop()

--- The Rust Programming Language Book/smart_pointers/src/bin/example6.rs
+++ The Rust Programming Language Book/smart_pointers/src/bin/example6.rs
@@ -1,6 +1,5 @@
 // Dropping a Value Early with std::mem::drop
 
-
 struct CustomerSmartPointer {
     data: String,
 }
@@ -12,10 +11,14 @@ impl Drop for CustomerSmartPointer {
 }
 
 fn main() {
-   let c = CustomerSmartPointer { data: String::from("my stuff")} ;
-   println!("CustomerSmartPointer c  created.");
-   // c.drop() destructor exists, but that explicit call isn't allowed.
-   std::mem::drop(c); 
-   let d = CustomerSmartPointer{ data: String::from("other stuff")};
-   println!("CustomerSmartPointer d created.");
+    let c = CustomerSmartPointer {
+        data: String::from("my stuff"),
+    };
+    println!("CustomerSmartPointer c  created.");
+    // c.drop() destructor exists, but that explicit call isn't allowed.
+    std::mem::drop(c);
+    let d = CustomerSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomerSmartPointer d created.");
 }
