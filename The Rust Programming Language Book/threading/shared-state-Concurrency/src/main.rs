be2b4076fbb675cd0c2d0e6ed7ac349116455295
Gaurav <gaurav.ssnl@gmail.com>
2020-06-11 01:08:22 +0530

    Mutex & Atomic Reference Counted Type example Changed folder name

--- The Rust Programming Language Book/threading/shared-state-Concurrency/src/main.rs
+++ The Rust Programming Language Book/threading/shared-state-Concurrency/src/main.rs
@@ -1,7 +1,25 @@
+use std::sync::Arc; // atomic reference counted type
 use std::sync::Mutex;
-use std::sync::thread;
+use std::thread;
 
 fn main() {
-    let counter = Mutex::mew(0);
+    // Mutex<T> provides interior mutability, as the Cell family does.
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
+
+    for _ in 0..10 {
+        // shadow count variable & use clone to have multiple owners of count variable
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for  handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", counter.lock().unwrap());
 }
