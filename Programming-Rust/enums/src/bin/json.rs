0e0c4b6213c936205a7224e57775b172f8560a84
Gaurav <gauravssnl@gmail.com>
2021-09-19 23:30:20 +0530

    Add sample Json enum.

---
+++ Programming-Rust/enums/src/bin/json.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+enum Json {
+    Null,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Json>),
+    Object(Box<HashMap<String, Json>>)
+}
+
+fn main() {
+
+}
