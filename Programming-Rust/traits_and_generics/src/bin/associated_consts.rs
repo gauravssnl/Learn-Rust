e661c4e548b7b781a889ea0c91b21c4ba864fd91
Gaurav <gaurav.ssnl@gmail.com>
2021-10-02 23:23:17 +0530

    Add more associated consts usages.

--- Programming-Rust/traits_and_generics/src/bin/associated_consts.rs
+++ Programming-Rust/traits_and_generics/src/bin/associated_consts.rs
@@ -1,5 +1,5 @@
 trait Greet {
-    const GREETING : &'static str = "Hello";
+    const GREETING: &'static str = "Hello";
     fn greet(&self) -> String;
 }
 
@@ -23,11 +23,28 @@ struct DummyStr;
 
 impl Greet for DummyStr {
     fn greet(&self) -> String {
-       <Self as Greet>::GREETING.to_string()
+        <Self as Greet>::GREETING.to_string()
     }
 }
+
 fn main() {
     println!("f32 ZERO = {}, ONE = {}", f32::ZERO, f64::ONE);
     println!("f642 ZERO = {}, ONE = {}", f64::ZERO, f64::ONE);
     println!("Greet trait's GREET: {}", DummyStr::GREETING);
+    println!("{}", add_one(2.5));
+    println!("{}", fib::<f64>(10));
+}
+
+use std::ops::Add;
+
+fn add_one<T: Float + Add<Output = T>>(value: T) -> T {
+    value + T::ONE
+}
+
+fn fib<T: Float + Add<Output = T>>(n: usize) -> T {
+    match n {
+        0 => T::ZERO,
+        1 => T::ONE,
+        n => fib::<T>(n - 1) + fib::<T>(n - 2),
+    }
 }
