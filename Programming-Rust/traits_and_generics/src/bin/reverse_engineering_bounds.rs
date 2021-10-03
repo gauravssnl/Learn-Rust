8b347c200c1bd1b2083ff7673076874b7d8cc8a1
Gaurav <gaurav.ssnl@gmail.com>
2021-10-03 13:51:43 +0530

    Add num crate example to reverse engineer bounds.

--- Programming-Rust/traits_and_generics/src/bin/reverse_engineering_bounds.rs
+++ Programming-Rust/traits_and_generics/src/bin/reverse_engineering_bounds.rs
@@ -26,6 +26,16 @@ fn dot1<N: Default + Add<Output = N> + Mul<Output = N> + Copy>(v1: &[N], v2: &[N
     total
 }
 
+use num::Num;
+
+fn dot2<N: Num + Copy>(v1: &[N], v2: &[N]) -> N {
+    let mut total = N::zero();
+    for i in 0..v1.len() {
+        total = total + v1[i] * v2[i];
+    }
+    total
+}
+
 fn main() {
     let v1 = [1, 2, 10, 16, 17];
     let v2 = [3, 6, 8, 9, 20];
@@ -34,4 +44,8 @@ fn main() {
     let v1 = [1, 2, 10, 16, 17];
     let v2 = [3, 6, 8, 9, 20];
     println!("{}", dot1(&v1, &v2));
+
+    let v1 = [1, 2, 10, 16, 17];
+    let v2 = [3, 6, 8, 9, 20];
+    println!("{}", dot2(&v1, &v2));
 }
