ea48899fce94b31d6e0c2c201cd75a9de193db6b
Gaurav <gaurav.ssnl@gmail.com>
2021-10-03 22:15:15 +0530

    Add PartialOrd, Reverse examples.

--- Programming-Rust/operator_overloading/src/main.rs
+++ Programming-Rust/operator_overloading/src/main.rs
@@ -1,5 +1,36 @@
 mod lib;
+use lib::Interval;
 
 fn main() {
-    println!("Hello, world!");
+    assert!(
+        Interval {
+            lower: 10,
+            upper: 20
+        } < Interval {
+            lower: 20,
+            upper: 40
+        }
+    );
+
+    let mut intervals = vec![
+        Interval {
+            lower: 10,
+            upper: 20,
+        },
+        Interval {
+            lower: 20,
+            upper: 40,
+        },
+        Interval {
+            lower: 5,
+            upper: 10,
+        },
+    ];
+
+    intervals.sort_by_key(|i| i.upper);
+    println!("intervals: {:?}", intervals);
+    // Ord is used in sorting
+    // Reverse wrapper takes adavtange of Ord that simply inverts any ordering.
+    intervals.sort_by_key(|i| std::cmp::Reverse(i.upper));
+    println!("intervals: {:?}", intervals);
 }
