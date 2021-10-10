43bf28ea7b334badab89f0817b036e08740624c7
Gaurav <gaurav.ssnl@gmail.com>
2021-10-11 00:03:53 +0530

    Add states and receiving inputs examples.

--- Fullstack-Rust/messages-actix/src/lib.rs
+++ Fullstack-Rust/messages-actix/src/lib.rs
@@ -1,8 +1,19 @@
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
-use serde::Serialize;
+use actix_web::{middleware, web, App, HttpServer, Result};
+use serde::{Deserialize, Serialize};
+use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+
+static SERVER_COUNETR: AtomicUsize = AtomicUsize::new(0);
+
+struct AppState {
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>,
+}
 
 pub struct MessageApp {
     host: String,
@@ -16,10 +27,22 @@ impl MessageApp {
 
     pub fn run(&self) -> std::io::Result<()> {
         println!("Starting http server: {}:{}", self.host, self.port);
+        let messages = Arc::new(Mutex::new(vec![]));
         HttpServer::new(move || {
             App::new()
+                .data(AppState {
+                    server_id: SERVER_COUNETR.fetch_add(1, Ordering::SeqCst),
+                    request_count: Cell::new(0),
+                    messages: messages.clone(),
+                })
                 .wrap(middleware::Logger::default())
                 .service(index)
+                .service(
+                    web::resource("/send")
+                        .data(web::JsonConfig::default().limit(4096))
+                        .route(web::post().to(post)),
+                )
+                .service(clear)
         })
         .bind((self.host.to_owned(), self.port))?
         .workers(8)
@@ -29,17 +52,59 @@ impl MessageApp {
 
 #[derive(Serialize)]
 struct IndexRespone {
+    server_id: usize,
+    request_count: usize,
+    messages: Vec<String>,
+}
+
+#[derive(Deserialize)]
+pub struct PostInput {
+    message: String,
+}
+
+#[derive(Serialize)]
+struct PostResponse {
+    server_id: usize,
+    request_count: usize,
     message: String,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexRespone>> {
-    let hello = req
-        .headers()
-        .get("hello")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexRespone>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let ms = state.messages.lock().unwrap();
+
     Ok(web::Json(IndexRespone {
-        message: hello.to_owned(),
+        server_id: state.server_id,
+        request_count,
+        messages: ms.clone(),
+    }))
+}
+
+fn post(msg: web::Json<PostInput>, state: web::Data<AppState>) -> Result<web::Json<PostResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.push(msg.message.clone());
+
+    Ok(web::Json(PostResponse {
+        server_id: state.server_id,
+        request_count: request_count,
+        message: msg.message.clone(),
+    }))
+}
+
+#[post("/clear")]
+fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexRespone>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.clear();
+
+    Ok(web::Json(IndexRespone {
+        server_id: state.server_id,
+        request_count,
+        messages: vec![],
     }))
 }
